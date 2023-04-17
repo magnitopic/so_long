a3cbc1e64dec56ddbdf9ecde16eeb686eb2f8852
magnitopic <magnitopic@gmail.com>
2023-04-17 19:11:02 +0200

    new icons and new folder name

--- src/so_long.c
+++ src/so_long.c
@@ -6,22 +6,20 @@
 /*   By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 15:16:02 by alaparic          #+#    #+#             */
-/*   Updated: 2023/04/17 16:09:57 by alaparic         ###   ########.fr       */
+/*   Updated: 2023/04/17 19:07:18 by alaparic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
-static int	close_window(int key, void *game)
+static int	close_window(int key, t_mlx *game)
 {
 	(void)key;
-	//game = (t_mlx *)game;
-	mlx_destroy_window(((t_mlx *)game)->mlx, ((t_mlx *)game)->win);
+	mlx_destroy_window(game->mlx, game->win);
 	exit(0);
-	return (0);
 }
 
-static int	event_handler(int key, void *game)
+static int	event_handler(int key, t_mlx *game)
 {
 	if (key == 53)
 		close_window(key, game);
@@ -36,27 +34,36 @@ static int	event_handler(int key, void *game)
 	return (0);
 }
 
-int	main(void)
+static void	put_sprites(t_mlx *game)
 {
-	t_mlx	*game;
 	void	*img;
 	int		img_width;
 	int		img_height;
 
+	img = mlx_xpm_file_to_image(game->mlx, "./textures/salida.xpm", &img_width, &img_height);
+	mlx_put_image_to_window(game->mlx, game->win, img, 64*2, 64*2);
+	img = mlx_xpm_file_to_image(game->mlx, "./textures/wall.xpm", &img_width, &img_height);
+	mlx_put_image_to_window(game->mlx, game->win, img, 64, 0);
+	mlx_put_image_to_window(game->mlx, game->win, img, 64, 64*2);
+	mlx_put_image_to_window(game->mlx, game->win, img, 0, 64);
+	img = mlx_xpm_file_to_image(game->mlx, "./textures/player.xpm", &img_width, &img_height);
+	mlx_put_image_to_window(game->mlx, game->win, img, 0, 64*2);
+	img = mlx_xpm_file_to_image(game->mlx, "./textures/coin.xpm", &img_width, &img_height);
+	mlx_put_image_to_window(game->mlx, game->win, img, 64*0, 64*0);
+}
+
+int	main(void)
+{
+	t_mlx	*game;
+
 	game = malloc(sizeof(t_mlx));
 	game->mlx = mlx_init();
-	game->win = mlx_new_window(game->mlx, 64 * 20, 64 * 20, "so_long");
-	img = mlx_xpm_file_to_image(game->mlx, "./img/salida.xpm", &img_width, &img_height);
-	mlx_put_image_to_window(game->mlx, game->win, img, 0, 0);
-	img = mlx_xpm_file_to_image(game->mlx, "./img/wall.xpm", &img_width, &img_height);
-	mlx_put_image_to_window(game->mlx, game->win, img, 64, 0);
-	mlx_put_image_to_window(game->mlx, game->win, img, 64, 64);
-	mlx_put_image_to_window(game->mlx, game->win, img, 0, 64);
-	img = mlx_xpm_file_to_image(game->mlx, "./img/player.xpm", &img_width, &img_height);
-	mlx_put_image_to_window(game->mlx, game->win, img, 0, 64*2);
-	img = mlx_xpm_file_to_image(game->mlx, "./img/coin.xpm", &img_width, &img_height);
-	mlx_put_image_to_window(game->mlx, game->win, img, 64, 64*2);
-	mlx_hook(game->win, 17, 0, close_window, game);
+	game->win = mlx_new_window(game->mlx, 64 * 15, 64 * 15, "so_long");
+	// img drawing
+	put_sprites(game);
+	// Events
 	mlx_hook(game->win, 2, 1L << 0, event_handler, game);
+	mlx_hook(game->win, 17, 0L, close_window, game);
+
 	mlx_loop(game->mlx);
 }
