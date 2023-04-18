3061407cb16001a36830266bfdda8e236154349f
magnitopic <magnitopic@gmail.com>
2023-04-18 19:09:58 +0200

    trying to read maps

--- include/so_long.h
+++ include/so_long.h
@@ -6,7 +6,7 @@
 /*   By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/14 16:16:33 by alaparic          #+#    #+#             */
-/*   Updated: 2023/04/14 18:21:30 by alaparic         ###   ########.fr       */
+/*   Updated: 2023/04/18 16:24:13 by alaparic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,14 +16,10 @@
 # include <mlx.h>
 # include "../libft/libft.h"
 
-typedef struct s_mlx
+typedef struct s_game
 {
 	void	*mlx;
 	void	*win;
-}	t_mlx;
-
-typedef struct s_game
-{
 	char	**map;
 }	t_game;
 
--- src/so_long.c
+++ src/so_long.c
@@ -6,20 +6,20 @@
 /*   By: alaparic <alaparic@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/11 15:16:02 by alaparic          #+#    #+#             */
-/*   Updated: 2023/04/17 19:07:18 by alaparic         ###   ########.fr       */
+/*   Updated: 2023/04/18 19:09:24 by alaparic         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/so_long.h"
 
-static int	close_window(int key, t_mlx *game)
+static int	close_window(int key, t_game *game)
 {
 	(void)key;
 	mlx_destroy_window(game->mlx, game->win);
 	exit(0);
 }
 
-static int	event_handler(int key, t_mlx *game)
+static int	event_handler(int key, t_game *game)
 {
 	if (key == 53)
 		close_window(key, game);
@@ -34,7 +34,7 @@ static int	event_handler(int key, t_mlx *game)
 	return (0);
 }
 
-static void	put_sprites(t_mlx *game)
+static void	put_sprites(t_game *game)
 {
 	void	*img;
 	int		img_width;
@@ -46,17 +46,62 @@ static void	put_sprites(t_mlx *game)
 	mlx_put_image_to_window(game->mlx, game->win, img, 64, 0);
 	mlx_put_image_to_window(game->mlx, game->win, img, 64, 64*2);
 	mlx_put_image_to_window(game->mlx, game->win, img, 0, 64);
-	img = mlx_xpm_file_to_image(game->mlx, "./textures/player.xpm", &img_width, &img_height);
-	mlx_put_image_to_window(game->mlx, game->win, img, 0, 64*2);
 	img = mlx_xpm_file_to_image(game->mlx, "./textures/coin.xpm", &img_width, &img_height);
 	mlx_put_image_to_window(game->mlx, game->win, img, 64*0, 64*0);
+	img = mlx_xpm_file_to_image(game->mlx, "./textures/space.xpm", &img_width, &img_height);
+	mlx_put_image_to_window(game->mlx, game->win, img, 64*1, 64*1);
+	mlx_put_image_to_window(game->mlx, game->win, img, 0, 64*2);
+	img = mlx_xpm_file_to_image(game->mlx, "./textures/player.xpm", &img_width, &img_height);
+	mlx_put_image_to_window(game->mlx, game->win, img, 0, 64*2);
 }
 
-int	main(void)
+// ! get-next-lie already used here and need a different system
+int	map_len(int fd)
 {
-	t_mlx	*game;
+	char	*line;
+	int		len;
 
-	game = malloc(sizeof(t_mlx));
+	len = 0;
+	line = get_next_line(fd);
+	while (line != NULL)
+	{
+		free(line);
+		line = get_next_line(fd);
+		len++;
+	}
+	free(line);
+	return (len);
+}
+
+void	read_map(char *argv, t_game *game)
+{
+	int		i;
+	int		fd;
+	char	**map;
+
+	fd = open(argv, O_RDONLY);
+	map = malloc((map_len(fd) + 1));
+	i = 0;
+	map[i] = get_next_line(fd);
+	while (map[i] != NULL)
+	{
+		map[i] = get_next_line(fd);
+		i++;
+		ft_printf("I: %d", i);
+	}
+	map[i] = NULL;
+	ft_printf("%s", *map);
+	game->map = map;
+}
+
+int	main(int argc, char **argv)
+{
+	t_game	*game;
+
+	if (argc != 2)
+		return (0);
+	game = malloc(sizeof(t_game));
+	read_map(argv[1], game);
 	game->mlx = mlx_init();
 	game->win = mlx_new_window(game->mlx, 64 * 15, 64 * 15, "so_long");
 	// img drawing
